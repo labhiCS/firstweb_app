5e425100c329c8c037cef2f4918a47d900230a8b
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-10 20:51:53 +0545

    First django project

---
+++ web_apps/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'web_apps'
+urlpatterns = [
+    # Home page.
+    path('', views.index, name='index'),
+     # Show all topics.
+    path('topics/', views.topics, name='topics'),
+     # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
+
---
+++ web_apps/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from .models import Topic
+
+# Create your views here.
+def index(request):
+    """The home page for web app"""
+    return render(request, 'web_apps/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'web_apps/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'web_appss/topic.html', context)
